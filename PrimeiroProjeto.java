99a359fd64f48dac4c91e3b1b435c93df6508fda
Hayanna Castro <152522247+hayhay07@users.noreply.github.com>
2025-03-18 08:47:21 -0300

    Create PrimeiroProjeto.java

---
+++ PrimeiroProjeto.java
@@ -0,0 +1,52 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int i = 34;
+		int j = 98;
+		double x = 2.8;
+		double y = 9.4;
+		
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(x);
+		System.out.println(y);
+		
+		System.out.println(" ");
+		System.out.println("Adição...");
+		System.out.println("i + j = " + (i + j));
+		System.out.println("i + x = " + (i + x));
+		System.out.println("y + j = " + (y + j));
+		System.out.println("x + j = " + (x + j));
+		
+		System.out.println(" ");
+		System.out.println("Subtração...");
+		System.out.println("i - j = " + (i - j));
+		System.out.println("i - x = " + (i - x));
+		System.out.println("y - j = " + (y - j));
+		System.out.println("x - j = " + (x - j));
+		
+		System.out.println(" ");
+		System.out.println("Divisão...");
+		System.out.println("i / j = " + (i / j));
+		System.out.println("i / x = " + (i / x));
+		System.out.println("y / j = " + (y / j));
+		System.out.println("x / j = " + (x / j));
+		
+		System.out.println(" ");
+		System.out.println("Multiplicação...");
+		System.out.println("i * j = " + (i * j));
+		System.out.println("i * x = " + (i * x));
+		System.out.println("y * j = " + (y * j));
+		System.out.println("x * j = " + (x * j));
+		
+		System.out.println(" ");
+		System.out.println("Resto...");
+		System.out.println("i % j = " + (i % j));
+		System.out.println("i % x = " + (i % x));
+		System.out.println("y % j = " + (y % j));
+		System.out.println("x % j = " + (x % j));
+	}
+
+}
