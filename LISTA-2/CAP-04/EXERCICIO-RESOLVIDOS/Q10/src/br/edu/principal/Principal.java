6aeca9ec6520a0ae114c563561562d257887d528
Hayanna Castro <152522247+hayhay07@users.noreply.github.com>
2025-04-22 13:01:47 -0300

    Update Principal.java

--- LISTA-2/CAP-04/EXERCICIO-RESOLVIDOS/Q10/src/br/edu/principal/Principal.java
+++ LISTA-2/CAP-04/EXERCICIO-RESOLVIDOS/Q10/src/br/edu/principal/Principal.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        // Criando o objeto Scanner para ler entradas do usuário
+        
         Scanner scanner = new Scanner(System.in);
 
-        // Variáveis para armazenar os dados das duas datas
+       
         int d1, m1, a1, d2, m2, a2;
 
-        // Solicita a primeira data
+    
         System.out.println("Digite a primeira data:");
         System.out.print("Dia (dd): ");
         d1 = scanner.nextInt();
@@ -19,7 +19,7 @@ public class Principal {
         System.out.print("Ano (aaaa): ");
         a1 = scanner.nextInt();
 
-        // Solicita a segunda data
+     
         System.out.println("Digite a segunda data:");
         System.out.print("Dia (dd): ");
         d2 = scanner.nextInt();
@@ -28,7 +28,7 @@ public class Principal {
         System.out.print("Ano (aaaa): ");
         a2 = scanner.nextInt();
 
-        // Comparando as duas datas
+     
         if (a1 > a2) {
             System.out.println("A maior data é: " + d1 + "-" + m1 + "-" + a1);
         } else if (a2 > a1) {
@@ -45,7 +45,7 @@ public class Principal {
             System.out.println("As datas são iguais!");
         }
 
-        // Fechando o scanner
+
         scanner.close();
     }
 }
