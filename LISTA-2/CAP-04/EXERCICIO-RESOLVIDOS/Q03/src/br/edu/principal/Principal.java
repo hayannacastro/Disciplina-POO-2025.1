f72870b9858b989909105ca56ff4f4687a7e1d8d
Hayanna Castro <152522247+hayhay07@users.noreply.github.com>
2025-04-22 12:58:25 -0300

    Update Principal.java

--- LISTA-2/CAP-04/EXERCICIO-RESOLVIDOS/Q03/src/br/edu/principal/Principal.java
+++ LISTA-2/CAP-04/EXERCICIO-RESOLVIDOS/Q03/src/br/edu/principal/Principal.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        // Scanner para entrada de dados
+       
         Scanner scanner = new Scanner(System.in);
 
-        // Declaração das variáveis
+  
         double num1, num2;
 
-        // Entrada dos números
+    
         System.out.print("Digite o primeiro número: ");
         num1 = scanner.nextDouble();
 
         System.out.print("Digite o segundo número: ");
         num2 = scanner.nextDouble();
 
-        // Verificação e exibição do maior número
+       
         if (num1 > num2) {
             System.out.println("O maior número é: " + num1);
         } else if (num2 > num1) {
@@ -26,7 +26,7 @@ public class Principal {
             System.out.println("Os números são iguais.");
         }
 
-        // Fechando o scanner
+
         scanner.close();
     }
 }
