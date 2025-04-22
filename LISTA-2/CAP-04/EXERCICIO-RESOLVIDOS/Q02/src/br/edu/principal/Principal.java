80d6dc085918852e66398805e25bc23080124146
Hayanna Castro <152522247+hayhay07@users.noreply.github.com>
2025-04-22 12:58:02 -0300

    Update Principal.java

--- LISTA-2/CAP-04/EXERCICIO-RESOLVIDOS/Q02/src/br/edu/principal/Principal.java
+++ LISTA-2/CAP-04/EXERCICIO-RESOLVIDOS/Q02/src/br/edu/principal/Principal.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        // Scanner para entrada de dados
+      
         Scanner scanner = new Scanner(System.in);
 
-        // Declaração das variáveis
+        
         double nota1, nota2, nota3, media, notaExame;
 
-        // Entrada das notas
+      
         System.out.print("Digite a primeira nota: ");
         nota1 = scanner.nextDouble();
 
@@ -20,13 +20,13 @@ public class Principal {
         System.out.print("Digite a terceira nota: ");
         nota3 = scanner.nextDouble();
 
-        // Cálculo da média aritmética
+        
         media = (nota1 + nota2 + nota3) / 3;
 
-        // Exibição da média
+      
         System.out.printf("Média aritmética: %.2f\n", media);
 
-        // Verificação da situação do aluno
+        
         if (media >= 0 && media < 3) {
             System.out.println("Reprovado");
         } else if (media >= 3 && media < 7) {
