4918e4d6a25667772b2c45bfecf2a0cb4af9c007
Hayanna Castro <152522247+hayhay07@users.noreply.github.com>
2025-04-24 13:27:54 -0300

    Update Principal.java

--- LISTA-2/CAP-05/EXERCICIO-RESOLVIDOS/Q04/src/br/edu/principal/Principal.java
+++ LISTA-2/CAP-05/EXERCICIO-RESOLVIDOS/Q04/src/br/edu/principal/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Declaração das variáveis
+       
         int cod, numVeiculos, numAcidentes;
         int maior = 0, menor = Integer.MAX_VALUE;
         int cidMaior = 0, cidMenor = 0;
@@ -14,9 +14,9 @@ public class Principal {
         int somaAcidentes = 0;
         int contAcidentes = 0;
 
-        // Laço para ler os dados das 5 cidades
+       
         for (int cont = 1; cont <= 5; cont++) {
-            // Leitura dos dados da cidade
+          
             System.out.print("Digite o código da cidade: ");
             cod = scanner.nextInt();
             System.out.print("Digite o número de veículos de passeio: ");
@@ -24,7 +24,7 @@ public class Principal {
             System.out.print("Digite o número de acidentes com vítimas: ");
             numAcidentes = scanner.nextInt();
 
-            // Calcular o maior e o menor índice de acidentes
+    
             if (cont == 1) {
                 maior = numAcidentes;
                 cidMaior = cod;
@@ -41,25 +41,25 @@ public class Principal {
                 }
             }
 
-            // Somar o total de veículos
+            
             somaVeiculos += numVeiculos;
 
-            // Somar o total de acidentes nas cidades com menos de 2000 veículos
+           
             if (numVeiculos < 2000) {
                 somaAcidentes += numAcidentes;
                 contAcidentes++;
             }
         }
 
-        // Exibindo os resultados
+      
         System.out.println("Maior índice de acidentes: " + maior + " na cidade de " + cidMaior);
         System.out.println("Menor índice de acidentes: " + menor + " na cidade de " + cidMenor);
 
-        // Calculando a média de veículos
+        
         double mediaVeiculos = (double) somaVeiculos / 5;
         System.out.println("Média de veículos nas 5 cidades: " + mediaVeiculos);
 
-        // Calculando a média de acidentes nas cidades com menos de 2000 veículos
+        
         if (contAcidentes == 0) {
             System.out.println("Não foi digitada nenhuma cidade com menos de 2000 veículos.");
         } else {
@@ -67,7 +67,7 @@ public class Principal {
             System.out.println("Média de acidentes nas cidades com menos de 2000 veículos: " + mediaAcidentes);
         }
 
-        // Fechar o scanner
+        
         scanner.close();
     }
 }
