3722ecc074707adb024ea8d5fe509a2231bb3fbf
Hayanna Castro <152522247+hayhay07@users.noreply.github.com>
2025-04-24 13:29:55 -0300

    Update Principal.java

--- LISTA-2/CAP-05/EXERCICIO-RESOLVIDOS/Q06/src/br/edu/principal/Principal.java
+++ LISTA-2/CAP-05/EXERCICIO-RESOLVIDOS/Q06/src/br/edu/principal/Principal.java
@@ -6,12 +6,12 @@ public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Definindo o salário mínimo
+        
         double salarioMinimo = 450.00;
 
-        // Processando os dados de 10 funcionários
+       
         for (int cont = 1; cont <= 10; cont++) {
-            // Leitura dos dados do funcionário
+            
             System.out.println("Digite os dados do funcionário " + cont);
             System.out.print("Código: ");
             int codigo = scanner.nextInt();
@@ -22,19 +22,19 @@ public class Principal {
             System.out.print("Categoria (G - gerente, O - operário): ");
             char categoria = scanner.next().charAt(0);
 
-            // Validação do turno
+           
             while (turno != 'M' && turno != 'V' && turno != 'N') {
                 System.out.print("Turno inválido. Digite novamente (M, V ou N): ");
                 turno = scanner.next().charAt(0);
             }
 
-            // Validação da categoria
+           
             while (categoria != 'G' && categoria != 'O') {
                 System.out.print("Categoria inválida. Digite novamente (G ou O): ");
                 categoria = scanner.next().charAt(0);
             }
 
-            // Cálculo do valor da hora trabalhada
+          
             double valorHora = 0.0;
             if (categoria == 'G') {
                 if (turno == 'N') {
@@ -50,10 +50,10 @@ public class Principal {
                 }
             }
 
-            // Cálculo do salário inicial
+           
             double salarioInicial = nht * valorHora;
 
-            // Cálculo do auxílio alimentação
+           
             double auxilioAlimentacao = 0.0;
             if (salarioInicial <= 300) {
                 auxilioAlimentacao = salarioInicial * 20 / 100;
@@ -63,10 +63,10 @@ public class Principal {
                 auxilioAlimentacao = salarioInicial * 5 / 100;
             }
 
-            // Cálculo do salário final
+           
             double salarioFinal = salarioInicial + auxilioAlimentacao;
 
-            // Exibição dos resultados
+            
             System.out.println("\nResumo do Funcionário:");
             System.out.println("Código: " + codigo);
             System.out.println("Número de horas trabalhadas: " + nht);
@@ -77,7 +77,7 @@ public class Principal {
             System.out.println("-------------------------------------\n");
         }
 
-        // Fechar o scanner
+        
         scanner.close();
     }
 }
