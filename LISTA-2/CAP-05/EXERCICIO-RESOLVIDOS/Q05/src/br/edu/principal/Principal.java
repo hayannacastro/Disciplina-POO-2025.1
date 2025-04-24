15e3f4164b178b529809c0a90f5f754677701ca5
Hayanna Castro <152522247+hayhay07@users.noreply.github.com>
2025-04-24 13:29:11 -0300

    Update Principal.java

--- LISTA-2/CAP-05/EXERCICIO-RESOLVIDOS/Q05/src/br/edu/principal/Principal.java
+++ LISTA-2/CAP-05/EXERCICIO-RESOLVIDOS/Q05/src/br/edu/principal/Principal.java
@@ -7,41 +7,41 @@ public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Leitura dos dados
+       
         System.out.print("Digite o número de termos da série: ");
         int numTermos = scanner.nextInt();
         System.out.print("Digite o valor de X: ");
         double x = scanner.nextDouble();
         
-        // Variáveis para cálculo
-        double s = 0; // Resultado da série
-        int denominador = 1; // Controla o sinal e o valor do fatorial
         
-        // Laço para calcular cada termo da série
+        double s = 0; 
+        int denominador = 1;
+        
+        
         for (int i = 1; i <= numTermos; i++) {
             int expoente = i + 1;
             double fat = 1;
             
-            // Calculando o fatorial
+            
             for (int j = 1; j <= expoente; j++) {
                 fat *= j;
             }
             
-            // Alternando os sinais e adicionando o termo à soma
+            
             if (expoente % 2 == 0) {
-                s -= Math.pow(x, expoente) / fat; // Sinal negativo para expoentes pares
+                s -= Math.pow(x, expoente) / fat; 
             } else {
-                s += Math.pow(x, expoente) / fat; // Sinal positivo para expoentes ímpares
+                s += Math.pow(x, expoente) / fat; 
             }
 
-            // Alterando o denominador (sinal) a cada 4 termos
+            
             if (denominador == 4) {
                 denominador = -1;
             } else if (denominador == 1) {
                 denominador = 1;
             }
 
-            // Alterna o sinal a cada 4 termos
+           
             if (denominador == 1) {
                 denominador = 2;
             } else {
@@ -49,10 +49,8 @@ public class Principal {
             }
         }
 
-        // Exibe o resultado da série
         System.out.println("O valor da série é: " + s);
         
-        // Fechar o scanner
         scanner.close();
     }
 }
